9ada31399612c60146098d0cdcd2d7a6a0382bc6
Mengleifeng <fmleing@163.com>
2020-03-07 21:02:50 +0800

    通过 FactoryBean 去实现 实例化

---
+++ src/main/java/com/feng/spring/bean/factory/UserFactoryBean.java
@@ -0,0 +1,16 @@
+package com.feng.spring.bean.factory;
+
+import com.feng.spring.bean.pojo.User;
+import org.springframework.beans.factory.FactoryBean;
+
+public class UserFactoryBean implements FactoryBean<User> {
+    @Override
+    public User getObject() throws Exception {
+        return User.createUser();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return User.class;
+    }
+}
