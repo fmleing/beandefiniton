c066a9aa45047e24e4ca2b403eb97efa8ce89dd6
Mengleifeng <fmleing@163.com>
2020-03-09 16:02:56 +0800

    通过 ServiceLoaderFactoryBean 去实现 实例化

---
+++ src/main/java/com/feng/spring/bean/definition/SpecialBeanInstantiationDemo.java
@@ -0,0 +1,36 @@
+package com.feng.spring.bean.definition;
+
+import com.feng.spring.bean.factory.UserFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+/**
+ * 特殊的 Bean 初始化
+ */
+public class SpecialBeanInstantiationDemo {
+
+    public static void main(String[] args) {
+
+        // 配置 xml 配置文件
+        // 启动 Spring 应用上下文
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/special-bean-instantiation-context.xml");
+
+        demoServiceLoader();
+
+    }
+
+    public static void demoServiceLoader(){
+        // service 加载
+        ServiceLoader<UserFactory> serviceLoader = ServiceLoader.load(UserFactory.class, Thread.currentThread()
+                .getContextClassLoader());
+        // 使用迭代器 进行迭代
+        Iterator<UserFactory> iterator = serviceLoader.iterator();
+        while (iterator.hasNext()) {
+            UserFactory userFactory = iterator.next();
+            System.out.println(userFactory.createUser());
+        }
+    }
+}
