fcdcf5b6f6bbcbb1a2bbca01203ece1076c7b923
Mengleifeng <fmleing@163.com>
2020-03-10 10:27:28 +0800

    基于 @PostConstant 初始化 示例

--- src/main/java/com/feng/spring/bean/definition/BeanInitializationDemo.java
+++ src/main/java/com/feng/spring/bean/definition/BeanInitializationDemo.java
@@ -1,5 +1,9 @@
 package com.feng.spring.bean.definition;
 
+import com.feng.spring.bean.factory.DefaultUserFacoty;
+import com.feng.spring.bean.factory.UserFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -7,4 +11,22 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration  // 配置 类
 public class BeanInitializationDemo {
+
+    public static void main(String[] args) {
+        // 创建 BeanFactory 容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        // 注册 Configuration 类
+        applicationContext.register(BeanInitializationDemo.class);
+        // 启动 applicationContext
+        applicationContext.refresh();
+
+        UserFactory userFactory = applicationContext.getBean(UserFactory.class);
+        // 关闭 applicationContext
+        applicationContext.close();
+    }
+
+    @Bean
+    public UserFactory initUserFactory(){
+        return new DefaultUserFacoty();
+    }
 }
