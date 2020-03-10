c753bcb766e53215a8b19053e3735817a6d98a4a
Mengleifeng <fmleing@163.com>
2020-03-10 10:22:08 +0800

    初始化 示例

---
+++ src/main/java/com/feng/spring/bean/definition/BeanInitializationDemo.java
@@ -0,0 +1,10 @@
+package com.feng.spring.bean.definition;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Bran 初始化 示例
+ */
+@Configuration  // 配置 类
+public class BeanInitializationDemo {
+}
--- src/main/java/com/feng/spring/bean/factory/DefaultUserFacoty.java
+++ src/main/java/com/feng/spring/bean/factory/DefaultUserFacoty.java
@@ -1,7 +1,15 @@
 package com.feng.spring.bean.factory;
 
+import javax.annotation.PostConstruct;
+
 /**
  * 默认 {@link UserFactory} 实现
  */
 public class DefaultUserFacoty implements  UserFactory {
+
+    // 1. 基于 @PostConstruct 注解
+    @PostConstruct
+    public void init(){
+        System.out.println(" @PostConstruct 初始化。。。 ");
+    }
 }
