f0bf6ac8c27a5163ac5a526f2708f2d632b8fd14
Mengleifeng <fmleing@163.com>
2020-04-11 20:03:30 +0800

    抛出 BeansException 异常

--- src/main/java/com/feng/spring/bean/definition/TypeSafetyDependencyLookupDemo.java
+++ src/main/java/com/feng/spring/bean/definition/TypeSafetyDependencyLookupDemo.java
@@ -3,6 +3,8 @@ package com.feng.spring.bean.definition;
 import com.feng.spring.bean.pojo.User;
 import com.feng.spring.dependency.lookup.ObjectProviderDemo;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -21,16 +23,27 @@ public class TypeSafetyDependencyLookupDemo {
         // 演示 BeanFactory#getBean()方法的安全性
         displayBeanFactoryGetBean(applicationContext);
 
+        displayObjectFactoryGetBean(applicationContext);
         // 关闭 容器
         applicationContext.close();
     }
 
+    private static void displayObjectFactoryGetBean(AnnotationConfigApplicationContext applicationContext) {
+        ObjectFactory<User> objectProvider = applicationContext.getBeanProvider(User.class);
+        printBeansException(()->objectProvider.getObject());
+    }
+
     private static void displayBeanFactoryGetBean(AnnotationConfigApplicationContext applicationContext) {
+        printBeansException(()->applicationContext.getBean(User.class));
+    }
+
+    private static void printBeansException(Runnable runnable){
         try {
-            applicationContext.getBean(User.class);
-        } catch (BeansException expection) {
+            runnable.run();
+            // 由于 BeansException 是 RuntimeException 因此不会被 Runnable.run() 捕获到
+        } catch (BeansException exception) {
             // 线程安全的，生产上不要使用，容易发生阻塞
-            expection.printStackTrace();
+            exception.printStackTrace();
         }
     }
 }
