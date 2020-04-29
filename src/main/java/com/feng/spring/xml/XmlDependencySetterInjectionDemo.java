2a09bdc72e846ce952463e7da280f76c04a6f2a2
Mengleifeng <fmleing@163.com>
2020-04-29 09:15:32 +0800

    资源以及读取xml资源逻辑

--- src/main/java/com/feng/spring/xml/XmlDependencySetterInjectionDemo.java
+++ src/main/java/com/feng/spring/xml/XmlDependencySetterInjectionDemo.java
@@ -1,7 +1,19 @@
 package com.feng.spring.xml;
 
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
 /**
  * 基于 XML 资源的依赖 Setter 方法注入示例
  */
 public class XmlDependencySetterInjectionDemo {
+
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(beanFactory);
+
+        String xmlResourcePath = "";
+        xmlReader.loadBeanDefinitions(xmlResourcePath);
+    }
 }
