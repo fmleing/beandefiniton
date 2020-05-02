1378e08282b2ee03f2981250cab7f3c5c3827cda
Mengleifeng <fmleing@163.com>
2020-05-02 10:50:54 +0800

    编写 UserHolder 类

---
+++ src/main/java/com/feng/spring/xml/UserHolder.java
@@ -0,0 +1,16 @@
+package com.feng.spring.xml;
+
+import com.feng.spring.bean.pojo.User;
+
+public class UserHolder {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
